8bcf19f670eb384009c7cf9aa789b0928245274e
Evan Comtesse <ComtesseE1@s2.rpn.ch>
2024-08-19 19:03:08 +0200

    Implémentation de la classe Animation

---
+++ Animation.cs
@@ -0,0 +1,47 @@
+﻿using Raylib_cs;
+namespace uniray_Project
+{
+    public unsafe class Animation
+    {
+        /// <summary>
+        /// Model animation
+        /// </summary>
+        private ModelAnimation anim;
+        /// <summary>
+        /// The current frame of the animation
+        /// </summary>
+        private int frame;
+        /// <summary>
+        /// The current frame of the animation
+        /// </summary>
+        public int Frame 
+        { 
+            get 
+            {
+                // Update the current frame due to frame call
+                if (frame < anim.FrameCount)
+                {
+                    frame++;
+                }
+                else frame = 0;
+                return frame; 
+            } 
+            set 
+            { 
+                frame = value;
+            } 
+        }
+        /// <summary>
+        /// Model animation
+        /// </summary>
+        public ModelAnimation Anim { get { return anim; } set { anim = value; } }
+        /// <summary>
+        /// Animation constructor
+        /// </summary>
+        /// <param name="animation"></param>
+        public Animation(ModelAnimation anim)
+        {
+            this.anim = anim;
+        }
+    }
+}
