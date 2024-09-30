88ff8b9895ee69995503cf24773c2e783ae05131
Comtesse Evan <evan.comtesse@rpn.ch>
2024-09-30 11:14:24 +0200

    fix: passed AudioCenter to static

--- audio/AudioCenter.cs
+++ audio/AudioCenter.cs
@@ -6,10 +6,10 @@ namespace uniray_Project
     public class AudioCenter
     {
         /// <summary>Dictionary of sounds</summary>
-        private Dictionary<string, Sound> sounds;
+        private static Dictionary<string, Sound> sounds;
 
         /// <summary>Dictionary of musics</summary>
-        private Dictionary<string, Music> musics;
+        private static Dictionary<string, Music> musics;
 
         /// <summary>AudioCenter constructor</summary>
         public AudioCenter()
@@ -21,14 +21,14 @@ namespace uniray_Project
 
         /// <summary>Plays a sound</summary>
         /// <param name="key">Dictionary key of the sound</param>
-        public void PlaySound(string key)
+        public static void PlaySound(string key)
         {
             Raylib.PlaySound(sounds[key]);
         }
 
         /// <summary>Plays a sound on loop</summary>
         /// <param name="key">Dictionary key of the sound</param>
-        public void PlaySoundLoop(string key)
+        public static void PlaySoundLoop(string key)
         {
             if (!IsSoundPlaying(sounds[key]))
             {
@@ -38,21 +38,21 @@ namespace uniray_Project
 
         /// <summary>Stops a playing sound</summary>
         /// <param name="key">Dictionary key of the sound</param>
-        public void StopSound(string key)
+        public static void StopSound(string key)
         {
             Raylib.StopSound(sounds[key]);
         }
 
         /// <summary>Plays a selected music</summary>
         /// <param name="key">Dictionary key of the music</param>
-        public void PlayMusic(string key)
+        public static void PlayMusic(string key)
         {
             PlayMusicStream(musics[key]);
         }
 
         /// <summary>Updates a selected music</summary>
         /// <param name="key">Dictionary key of the music</param>
-        public void UpdateMusic(string key)
+        public static void UpdateMusic(string key)
         {
             UpdateMusicStream(musics[key]);
         }
@@ -60,7 +60,7 @@ namespace uniray_Project
         /// <summary>Sets music volume</summary>
         /// <param name="key">Dictionary key of the music</param>
         /// <param name="volume">Volume to set</param>
-        public void SetMusicVolume(string key, float volume)
+        public static void SetMusicVolume(string key, float volume)
         {
             Raylib.SetMusicVolume(musics[key], volume);
         }
